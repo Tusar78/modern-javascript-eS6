34e76e6a40214ff03ce730014b43057a4eb2c3b8
Tusar <tusarahmed141@gmail.com>
2022-06-17 07:18:38 +0600

    You can write expression in template literal

--- Template-Literal/app.js
+++ Template-Literal/app.js
@@ -4,3 +4,6 @@ const roll = 326478;
 
 const output = `My name is ${name}. and my roll number is ${roll}`;
 console.log(output);
+
+const exp = `Two number summation: 10 + 5 = ${10 + 5}`;
+console.log(exp);
