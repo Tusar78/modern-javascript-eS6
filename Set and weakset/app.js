978f18c202c443a06f036d0a23c00cb0bafde53d
Tusar <tusarahmed141@gmail.com>
2022-06-17 08:00:59 +0600

    Simple array data structure

---
+++ Set and weakset/app.js
@@ -0,0 +1,11 @@
+// Explore a new data structure which is set
+
+// Array vs set
+
+// Array literal 
+const arrLiteral = [5, 6];
+const arrConstructor = new Array();
+
+// Added data in array dataStructure
+arrConstructor[0] = 'Tusar';
+arrConstructor[1] = 'Rakib';
