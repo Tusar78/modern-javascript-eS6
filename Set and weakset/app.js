9c863dced57f7070bbddc5e0f88e81fd8e524a7f
Tusar <tusarahmed141@gmail.com>
2022-06-17 11:09:12 +0600

    Make difference

--- Set and weakset/app.js
+++ Set and weakset/app.js
@@ -48,5 +48,6 @@ const a = new Set([1, 2, 3]);
 const b = new Set([4, 3, 2]);
 
 // const union = new Set([...a, ...b]);
-const intersection = new Set([...a].filter(x => b.has(x)));
-console.log(intersection);
+// const intersection = new Set([...a].filter(x => b.has(x)));
+const difference = new Set([...a].filter(x => !b.has(x))); 
+console.log(difference);
