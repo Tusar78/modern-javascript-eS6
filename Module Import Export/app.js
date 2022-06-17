4cd541426e1f9ebd89f3bb10ae9ee377f3f0278e
Tusar <tusarahmed141@gmail.com>
2022-06-17 06:50:28 +0600

    Exploring import export

---
+++ Module Import Export/app.js
@@ -0,0 +1,7 @@
+// import { myRoll, myName } from "./external.js"; // Named Import
+import { external } from "./external.js";
+
+// console.log(myRoll);
+// console.log(myName);
+
+console.log(external);
---
+++ Module Import Export/external.js
@@ -0,0 +1,5 @@
+export const myRoll = 326478;
+export const myName = 'Tusar';
+
+const PI = 3.14159;
+export default PI;
