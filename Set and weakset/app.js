1e7c5b133052eb3e7e8b3094724ac7ed25291293
Tusar <tusarahmed141@gmail.com>
2022-06-17 09:05:07 +0600

    Array or Set dataStructure

--- Set and weakset/app.js
+++ Set and weakset/app.js
@@ -2,14 +2,13 @@
 
 // Array vs set
 
-// Array literal 
+// Array literal
 const arrLiteral = [5, 6];
 const arrConstructor = new Array();
 
 // Added data in array dataStructure
-arrConstructor[0] = 'Tusar';
-arrConstructor[1] = 'Rakib';
-
+arrConstructor[0] = "Tusar";
+arrConstructor[1] = "Rakib";
 
 // Set constructor. Set can not have any literal syntax
 const mySet = new Set();
@@ -18,14 +17,19 @@ const mySet = new Set();
 // mySet.add('Tusar');
 
 // Chaining Add
-mySet.add(1).add(2).add(3).add('Tusar');
+mySet.add(1).add(2).add(3).add("Tusar");
 
 // Delete element
-mySet.delete('Tusar');
+mySet.delete("Tusar");
 
 // Element available or not
 // console.log(mySet.has('Tusar'));
 // mySet.clear();
-console.log(mySet.keys());
-console.log(mySet.values());
-console.log(mySet.entries());
+// console.log(mySet.keys());
+// console.log(mySet.values());
+// console.log(mySet.entries());
+
+// Array to Set
+const numbers = [1, 2, 3, 4, 5];
+const numbersSet = new Set(numbers);
+console.log(numbersSet);
