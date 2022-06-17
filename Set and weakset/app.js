1b4f37866ede07c6eb9f54a8ed02e9c2d77d880f
Tusar <tusarahmed141@gmail.com>
2022-06-17 08:18:46 +0600

    Set add data

--- Set and weakset/app.js
+++ Set and weakset/app.js
@@ -9,3 +9,11 @@ const arrConstructor = new Array();
 // Added data in array dataStructure
 arrConstructor[0] = 'Tusar';
 arrConstructor[1] = 'Rakib';
+
+
+// Set constructor. Set can not have any literal syntax
+const mySet = new Set();
+
+// Add data in Set dataStructure
+mySet.add('Tusar')
+console.log(mySet);
