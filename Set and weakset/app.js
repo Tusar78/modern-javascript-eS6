fa561dc3311e3293f1a2e24dffff4356c758ee3d
Tusar <tusarahmed141@gmail.com>
2022-06-17 11:06:50 +0600

    We can make intersection array in two array

--- Set and weakset/app.js
+++ Set and weakset/app.js
@@ -47,5 +47,6 @@ mySet.delete("Tusar");
 const a = new Set([1, 2, 3]);
 const b = new Set([4, 3, 2]);
 
-const union = new Set([...a, ...b]);
-console.log(union);
+// const union = new Set([...a, ...b]);
+const intersection = new Set([...a].filter(x => b.has(x)));
+console.log(intersection);
